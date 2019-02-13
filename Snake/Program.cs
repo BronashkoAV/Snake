d568b9411fc73c505712f3539385d951f38b2f11
BronashkoAV <BronashkoAV@gmail.com>
2019-02-13 14:25:43 +0300

    Создали класс "змейка"

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,21 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetBufferSize(80, 25);
+            int width, height;
+            Console.SetBufferSize(width = Console.WindowWidth, height = Console.WindowHeight);
 
             //Отрисовка рамки
+            HorizontalLine upLine = new HorizontalLine(0, width-1, 1, '+');
+            HorizontalLine downLine = new HorizontalLine(0, width-1, height-2, '+');
+            VerticalLine leftLine = new VerticalLine(0, 1, height-3, '+');
+            VerticalLine rightLine = new VerticalLine(width-1, 1, height-3, '+');
 
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
-            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
             upLine.Draw();
             downLine.Draw();
             leftLine.Draw();
             rightLine.Draw();
 
+            //Отрисовка точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
+
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
             Console.ReadKey();
         }
 
